01c980f3cd6bbbb407bde9cca9e0452c0bf0f644
wreckseal <42881537+wreckseal@users.noreply.github.com>
2026-02-12 13:48:08 -0700

    Basic javascript project assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/JavaScript_File.js
@@ -0,0 +1,9 @@
+var a = 3;      // Variable a is assigned the value 3
+var b = 4;      // Variable b is assigned the value 4
+var c = a * b;  // Variable c is assigned the result of the expression a * b, which is 12
+window.alert("3 * 4 = " + c);   // An alert box will display the result of the expression 3 * 4, which is 12
+
+var Sent1 = "This is the beginning of the string, " // Variable Sent1 is assigned a string value
+var Sent2 = "and this is the end of the string."    // Variable Sent2 is assigned a string value
+var Message = Sent1 + Sent2; // Variable <Message> is assigned the concatenation of Sent1 and Sent2
+document.write(Message);     // The value of Message is written to the document
